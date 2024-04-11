0da44189d8e2876d52225929d8ea11e828910d42
heger.valter@gmail.com <heger.valter@gmail.com>
2024-04-11 12:40:25 -0400

    https://www.javaguides.net/2023/08/library-management-system-project-using-spring-boot.html

---
+++ start_spring_io/demo/src/main/java/com/example/demo/UserService.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import net.javaguides.bankingapp.entity.User;
+import net.javaguides.bankingapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+}
